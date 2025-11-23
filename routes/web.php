23e105ed1d01c4d50571e131bda2494f75140092
Dannyj005 <danielajones341@gmail.com>
2025-11-23 15:57:28 +0300

    Add cafeteria UI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CafeteriaController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/cafeteria', [CafeteriaController::class, 'index'])->name('cafeteria.index');
-Route::get('/cafeteria/menu', [CafeteriaController::class, 'menu'])->name('cafeteria.menu');
-Route::post('/cafeteria/order', [CafeteriaController::class, 'order'])->name('cafeteria.order');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application.
+|
+*/
 
+// Homepage
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Authentication routes
+Auth::routes();
+
+// Home dashboard
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// Cafeteria routes
+Route::prefix('cafeteria')->group(function () {
+
+    // Cafeteria main page
+    Route::get('/', [CafeteriaController::class, 'index'])->name('cafeteria.index');
+
+    // Menu page
+    Route::get('/menu', [CafeteriaController::class, 'menu'])->name('cafeteria.menu');
+
+    // Place an order
+    Route::post('/order', [CafeteriaController::class, 'order'])->name('cafeteria.order');
+});
